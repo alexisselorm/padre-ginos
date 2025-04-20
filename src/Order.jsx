3d22fdcd0d8546e1ab3915020c811377bcd17760
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2025-04-20 18:14:04 +0300

    Hooks: useState

---
+++ src/Order.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import Pizza from "./Pizza";
+
+
+export default function Order() {
+
+  const [pizzaType, setPizzaType] = useState('pepperoni');
+  const [pizzaSize,setPizzaSize]=useState('M');
+  // const pizzaType = 'Pepperoni';
+  // const pizzaSize = 'M';
+  return (
+    <div className="order">
+      <h2>Create Order</h2>
+      <form action="">
+        <div>
+          <div>
+            <label htmlFor="pizza-type">Pizza Type</label>
+            <select name="pizza-type" value={pizzaType}
+            onChange={(e)=>setPizzaType(e.target.value)}>
+              <option value="pepperoni">The Pepperoni Pizza</option>
+              <option value="hawaiian">The Hawaiian Pizza</option>
+              <option value="big_meat">The Big Meat Pizza</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="pizza-size">Pizza Size</label>
+            <div>
+              <span>
+                <input checked={pizzaSize==='S'} type="radio" name="pizza-size" value='S' id='pizza-s'
+                onChange={(e)=>setPizzaSize(e.target.value)}  />
+              <label htmlFor="pizza-s">Small</label>
+              </span>
+              <span>
+                <input checked={pizzaSize==='M'} type="radio" name="pizza-size" value='M' id='pizza-m'
+                onChange={(e)=>setPizzaSize(e.target.value)}  />
+              <label htmlFor="pizza-m">Medium</label>
+              </span>
+              <span>
+                <input checked={pizzaSize==='L'} type="radio" name="pizza-size" value='L' id='pizza-l'
+                onChange={(e)=>setPizzaSize(e.target.value)}  />
+              <label htmlFor="pizza-l">Large</label>
+              </span>
+            </div>
+          </div>
+          <button type="submit">Add to Cart</button>
+          <div className="order-pizza">
+            <Pizza name="Pepperoni" description="Another pep pizza"
+            image={"/public/pizzas/pepperoni.webp"} />
+            <p>$13.36</p>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
