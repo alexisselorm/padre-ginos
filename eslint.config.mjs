2d46379b8787b42032ba7cce753e4ccff4dd7ecc
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2025-04-22 11:32:44 +0300

    Routing and Querying: Tanstack Query

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
 import reactPlugin from "eslint-plugin-react";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -15,6 +16,7 @@ export default [
     },
   },
   reactPlugin.configs.flat["jsx-runtime"],
+  ...pluginQuery.configs["flat/recommended"],
   {
     files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
     languageOptions: {
@@ -22,15 +24,15 @@ export default [
         ...globals.browser,
         ...globals.node,
       },
-      rules: {
-        "react/no-unescaped-entities": "off",
-        "react/prop-types": "off",
-      },
       parserOptions: {
         ecmaFeatures: {
           jsx: true,
         },
       },
+    },
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react/prop-types": "off",
     },
   },
   prettier,
